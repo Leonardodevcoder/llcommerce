1e9b18c97e7fe7a9c2ddf010e9bb00daf6877d33
Leonardo <leonardoluan2017@gmail.com>
2024-06-25 17:03:04 -0300

    ✨ feat: Adicionado GET products paged

--- src/main/java/com/devleonardo/llcommerce/services/ProductService.java
+++ src/main/java/com/devleonardo/llcommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devleonardo.llcommerce.dto.ProductDTO;
 import com.devleonardo.llcommerce.entities.Product;
 import com.devleonardo.llcommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
        Product product = repository.findById(id).get();
        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+       Page<Product> result = repository.findAll(pageable);
+       return result.map(ProductDTO::new);
     }
 }

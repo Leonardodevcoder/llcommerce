c4306d007b16cec9f847cb6a65157ce283187bd8
Leonardo <leonardoluan2017@gmail.com>
2024-06-26 12:05:48 -0300

    ✨ feat: Bean Validation

--- src/main/java/com/devleonardo/llcommerce/dto/ProductDTO.java
+++ src/main/java/com/devleonardo/llcommerce/dto/ProductDTO.java
@@ -1,12 +1,20 @@
 package com.devleonardo.llcommerce.dto;
 
 import com.devleonardo.llcommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 3, message = "Descrição precisa ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 

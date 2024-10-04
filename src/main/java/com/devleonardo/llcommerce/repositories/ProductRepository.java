309685af34240a7ee8d4844b198aea4cac913e6f
Leonardo <leonardoluan2017@gmail.com>
2024-10-04 10:43:27 -0300

    ✨ feat: Domain model User Role, seed

--- src/main/java/com/devleonardo/llcommerce/repositories/ProductRepository.java
+++ src/main/java/com/devleonardo/llcommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.devleonardo.llcommerce.repositories;
 
 import com.devleonardo.llcommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj " +
+           "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> seachByName(String name, Pageable pageable);
 }
--- src/main/java/com/devleonardo/llcommerce/services/ProductService.java
+++ src/main/java/com/devleonardo/llcommerce/services/ProductService.java
@@ -28,8 +28,8 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        Page<Product> result = repository.findAll(pageable);
+    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result = repository.seachByName(name, pageable);
         return result.map(ProductDTO::new);
     }
 
